55428fdc6232d9d0e36c4da595dc5b43ef539b5c
Gazi Akter <gaziakterdev@gmail.com>
2024-02-10 11:14:07 +0600

    Add New custom role

--- user-role-management.php
+++ user-role-management.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: User Role Management
  * Description: A user role management plugin
- * Version: 1.0.0
+ * Version: 4.0.0
  * Author: Gazi Akter
  * Author URI: http://gaziakter.com/
  * Plugin URI: http://gaziakter.com/plugin/user-role-management/
@@ -14,7 +14,7 @@
     }
 
     public function user_role_namagement_function(){
-
+        add_role('custom_role', 'Custom Role', ['blocked' => true]);
     }
  }
 
