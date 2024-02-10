019414b4ff1cca266dd47ac49637b4b0614c5dc7
Gazi Akter <gaziakterdev@gmail.com>
2024-02-10 14:53:12 +0600

    use capability

--- user-role-management.php
+++ user-role-management.php
@@ -11,15 +11,37 @@
  Class User_role_management{
     public function __construct(){
         add_action( 'init', array($this, 'user_role_namagement_function') );
+        add_filter( 'query_vars', array($this, 'query_vars_function') );
+        add_action( 'template_redirect', array($this, 'template_redirection_function') );
     }
 
     public function user_role_namagement_function(){
 
         //Create New Custom role
-        add_role('custom_role', 'Custom Role', ['blocked' => true]);
+        add_role('custom_role', 'Custom Role', array( 'blocked' => true ));
 
         //Create custom url link
         add_rewrite_rule( 'custom-role/?$', 'index.php?custom-role=1', 'top' );
+
+        //user ristraction
+        if(is_admin() && current_user_can( 'blocked' )){
+            wp_redirect( get_home_url().'/custom-role' );
+            die();
+
+        }
+    }
+
+    public function query_vars_function($query_vars){
+        $query_vars[] = 'custom-role';
+        return $query_vars;
+    }
+
+    public function template_redirection_function(){
+        $is_custom_url = intval(get_query_var('custom-role' ));
+        if($is_custom_url){
+            echo '<h2>This is custom role page</h2>';
+            die();
+        }
     }
  }
 
